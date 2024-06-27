a169d5ee45b53a05f3bfa9a7b636d9f89b0a1aae
Tantris235 <166976348+Tantris235@users.noreply.github.com>
2024-06-27 13:29:33 +0200

    beta version

--- cracker.py
+++ cracker.py
@@ -3,11 +3,11 @@ import os
 from time import sleep
 
 class Cracker:()
-def __init__(self, file):
-    self.file_name = file
-    if os.path.exiists(file):
-        print("File located!")
-    else:
-        print("File could not be found!")
+    def __init__(self, file):
+        self.file_name = file
+        if os.path.exiists(file):
+            self.passes = self.read_data(self.file_name)
+        else:
+            print("File could not be found!")
 
 cracker = Cracker("passes.txt")
